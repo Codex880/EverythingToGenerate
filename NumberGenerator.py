19a4ff1c16eee3cd1d0343de07776fe0d22696c2
Codex880 <77204108+Codex880@users.noreply.github.com>
2021-03-02 18:15:56 +0100

    Create NumberGenerator.py

---
+++ NumberGenerator.py
@@ -0,0 +1,5 @@
+#Number Generator
+import random n = random. random() print(n)
+import random n = random. randint(0,22) print(n)
+import random randomlist = [] for i in range(0,5): n = random. randint(1,30) randomlist. ...
+import random 
